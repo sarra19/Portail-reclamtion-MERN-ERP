dad3d1d123ee5b64c4ab077c1f76acb882146ad0
Sarra Ben Hamida <81316721+sarra19@users.noreply.github.com>
2025-02-27 21:11:22 +0100

    commit

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -5,7 +5,7 @@ const config = {
   user: process.env.DB_USER , 
   password: process.env.DB_PASSWORD ,
   server: process.env.DB_HOST , 
-  database:"Demo Database BC (24-0)" , 
+  database: process.env.DB_NAME , 
   options: {
     encrypt: false,
     trustServerCertificate: true,
