a770cd2ebf67986faef9ab717aba5528c93e15b9
Sarra Ben Hamida <81316721+sarra19@users.noreply.github.com>
2025-02-27 21:48:05 +0100

    commit

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -2,10 +2,10 @@ const sql = require("mssql");
 require('dotenv').config();
 
 const config = {
-  user: process.env.DB_USER , 
-  password: process.env.DB_PASSWORD ,
-  server: process.env.DB_HOST , 
-  database: process.env.DB_NAME , 
+  user: process.env.DB_USER || "sarra", // Fallback value
+  password: process.env.DB_PASSWORD || "0000", // Fallback value
+  server: process.env.DB_HOST || "SARRA\\BCDEMO", // Fallback value
+  database: process.env.DB_NAME || "Demo Database BC (24-0)", // Fallback value
   options: {
     encrypt: false,
     trustServerCertificate: true,
