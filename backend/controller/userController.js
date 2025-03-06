ca8371d19449fc253beb66b1c45c2f9f54e9de5f
Sarra Ben Hamida <81316721+sarra19@users.noreply.github.com>
2025-03-06 18:58:51 +0100

    commit

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -349,6 +349,7 @@ async function SignIn(req, res) {
         // Set the cookie and send the response
         res.cookie("token", token, tokenOptions).status(200).json({
             message: "Connexion réussie",
+            token,
             success: true,
         });
 
