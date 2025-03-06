9d5c634f098ad5bebba0c91397a0145c710132d2
Sarra Ben Hamida <81316721+sarra19@users.noreply.github.com>
2025-03-06 12:53:12 +0100

    commit

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use('/', indexRouter);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 const PORT = process.env.PORT || 8081;
 
 connectDB().then(() => {
